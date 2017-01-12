cde2b479a20b254c8361b26ff96fba876a019000
Maxim Khatskevich <maxim@khatskevi.ch>
2017-01-12 23:20:16 +0100

    Added basic unit test

--- Tst/Main.swift
+++ Tst/Main.swift
@@ -17,15 +17,42 @@ class Main: XCTestCase
 {
     struct GM: AppModel
     {
-        let v: Int = 5
+        var v: Int = 5
     }
+    
+    //===
+    
+    let disp = Dispatcher(GM())
+    
+    //===
     
     func testExample()
     {
-        let state = GM()
+        let ex = expectation(description: "After Action")
         
-        let _ = Dispatcher(state)
+        //===
         
-        //
+        disp.subscribe(self)
+            .onUpdate {
+                
+                print("The value -->> \($0.v)")
+                
+                if $0.v == 15
+                {
+                    ex.fulfill()
+                }
+            }
+        
+        disp.submit(act1, with: 10)
+        disp.submit(act1, with: 15)
+        
+        //===
+        
+        waitForExpectations(timeout: 3.0)
+    }
+    
+    func act1(_ params: ActionParams<Int, GM>) -> Mutation<GM>
+    {
+        return { $0.v = params.input }
     }
 }
