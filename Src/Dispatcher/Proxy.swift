facfbf8c60ca6eecc3bf4afa4d9f1235167a4e4f
Maxim Khatskevich <maxim@khatskevi.ch>
2017-02-22 22:28:22 +0100

    Introduced ‘DispatcherProxy’

---
+++ Src/Dispatcher/Proxy.swift
@@ -0,0 +1,54 @@
+//
+//  Proxy.swift
+//  MKHUniFlow
+//
+//  Created by Maxim Khatskevich on 2/22/17.
+//  Copyright © 2017 Maxim Khatskevich. All rights reserved.
+//
+//  https://en.wikipedia.org/wiki/Proxy_pattern
+
+import Foundation
+
+//===
+
+public
+typealias DispatcherProxy =
+(
+    subscribe: (AnyObject) -> SubscriptionBlank,
+    subscribeLater: (AnyObject) -> SubscriptionBlank,
+    unsubscribe: (AnyObject) -> Void,
+    submit: ActionGetterWrapped
+)
+
+//===
+
+
+public
+extension Dispatcher
+{
+    func proxy() -> DispatcherProxy
+    {
+        return (
+            subscribe: { self.subscribe($0) },
+            subscribeLater: { self.subscribe($0, updateNow: false) },
+            unsubscribe: self.unsubscribe,
+            submit: self.submit
+        )
+    }
+}
+
+//===
+
+public
+protocol DispatcherBindable: class
+{
+    func bind(with proxy: DispatcherProxy) -> Self
+}
+
+//===
+
+public
+protocol DispatcherInitializable: class
+{
+    init(with proxy: DispatcherProxy)
+}
