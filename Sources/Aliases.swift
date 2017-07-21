88b1ba9ebe9e8b29e857fce14ba9bc80b93ffaad
Maxim Khatskevich <maxim@khatskevi.ch>
2017-07-22 03:11:40 +0500

    Added prefixed aliases again

--- Sources/Aliases.swift
+++ Sources/Aliases.swift
@@ -22,56 +22,36 @@ typealias ActionBody = (
 
 //=== MARK: Prefixed versions
 
-//public
-//typealias UFLWrapped<Value> = Wrapped<Value>
-//
-//public
-//typealias UFLMutations<Value> = Mutations<Value>
-//
-//public
-//typealias UFLModelMutations = ModelMutations
-//
-//public
-//typealias UFLStateGetter<State: FeatureState> = StateGetter<State>
-//
-//public
-//typealias UFLActionGetter = ActionGetter
-//
-//public
-//typealias UFLActionBody = ActionBody
-//
-//public
-//typealias UFLAction = Action
-//
-//public
-//typealias UFLInitializationInto<S: FeatureState> = InitializationInto<S>
-//
-//public
-//typealias UFLActualizationOf<S: FeatureState> = ActualizationOf<S>
-//
-//public
-//typealias UFLTriggerOn = TriggerOn
-//
-//public
-//typealias UFLTransitionBetween<From, Into> = TransitionBetween<From, Into> where
-//    From: FeatureState,
-//    Into: FeatureState,
-//    From.ParentFeature == Into.ParentFeature
-//
-//public
-//typealias UFLDeinitializationFrom<S: FeatureState> = DeinitializationFrom<S>
-//
-////public
-////typealias UFLGenericAction = GenericAction
-//
-//public
-//typealias UFLFeature = Feature
-//
-//public
-//typealias UFLFeatureState = FeatureState
-//
-//public
-//typealias UFLSimpleState = SimpleState
-//
-//public
-//typealias UFLDispatcher = Dispatcher
+public
+typealias UFLWrapped<Value> = Wrapped<Value>
+
+public
+typealias UFLMutations<Value> = Mutations<Value>
+
+public
+typealias UFLModelMutations = ModelMutations
+
+public
+typealias UFLStateGetter<State: FeatureState> = StateGetter<State>
+
+public
+typealias UFLActionGetter = ActionGetter
+
+public
+typealias UFLActionBody = ActionBody
+
+public
+typealias UFLAction = Action
+
+public
+typealias UFLFeature = Feature
+
+public
+typealias UFLFeatureState = FeatureState
+
+public
+typealias UFLSimpleState = SimpleState
+
+public
+typealias UFLDispatcher = Dispatcher
+
