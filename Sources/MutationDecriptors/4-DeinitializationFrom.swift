78b874359a6cb8e89ca55181d1cd46eac913f8f9
Maxim Khatskevich <maxim@khatskevi.ch>
2022-06-13 00:40:31 +0200

    [FIX] minor mistake in `DeinitializationFrom` declaration

--- Sources/MutationDecriptors/4-DeinitializationFrom.swift
+++ Sources/MutationDecriptors/4-DeinitializationFrom.swift
@@ -29,7 +29,7 @@ import Foundation /// for access to `Date` type
 //---
 
 public
-struct DeinitializationFrom<Old: SomeState>
+struct DeinitializationFrom<Old: SomeState>: SomeMutationDecriptor
 {
     public
     let timestamp: Date
